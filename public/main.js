4b3e146a8daad692d2a115f270a091f438b624eb
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-11-16 16:44:55 -0500

    game is working with computer making random choices and incrementing score for both, No bout win or lose yet

--- public/main.js
+++ public/main.js
@@ -2,12 +2,84 @@ const $ = s => document.querySelector(s)
 const $$ = s => document.querySelectorAll(s)
 
 const handleButtonClick = (event) => {
+  console.log(event)
   const player = event.target.className
   const computer = getComputerMove()
   $('figure.player img').src = `/images/${player}.svg`
   $('figure.computer img').src = `/images/${computer}.svg`
+  //
+  // console.log('PLAYER IS', player)
+  // console.log('COMPUTER IS', computer)
 
-  // HINT: Check for win, lose or draw, then call `gameOver()` eventually.
+  if (player === 'rock' && computer === 'paper') {
+    const computerScoreText = $('.scores .computer').textContent
+    const computerScore = parseInt(computerScoreText)
+    const newComputerScore = computerScore + 1
+    $('.scores .computer').textContent = newComputerScore
+    console.log('computer wins with score', newComputerScore)
+  }
+
+  // if (player === 'rock' && computer === 'rock') {
+  //   console.log('draw')
+  // }
+
+  if (player === 'rock' && computer === 'scissors') {
+    // Get the player score as text
+    const playerScoreText = $('.scores .player').textContent
+
+    // Convert that to a number
+    const playerScore = parseInt(playerScoreText)
+
+    // Add one to it
+    const newPlayerScore = playerScore + 1
+
+    // Put it back on screen
+    $('.scores .player').textContent = newPlayerScore
+
+    console.log('player wins with score', newPlayerScore)
+  }
+
+  // if (player === 'paper' && computer === 'paper') {
+  //   console.log('draw')
+  // }
+
+  if (player === 'paper' && computer === 'rock') {
+    const playerScoreText = $('.scores .player').textContent
+    const playerScore = parseInt(playerScoreText)
+    const newPlayerScore = playerScore + 1
+    $('.scores .player').textContent = newPlayerScore
+    console.log('player wins with score', newPlayerScore)
+  }
+
+  if (player === 'paper' && computer === 'scissors') {
+    const computerScoreText = $('.scores .computer').textContent
+    const computerScore = parseInt(computerScoreText)
+    const newComputerScore = computerScore + 1
+    $('.scores .computer').textContent = newComputerScore
+    console.log('computer wins with score', newComputerScore)
+  }
+
+  if (player === 'scissors' && computer === 'paper') {
+    const computerScoreText = $('.scores .computer').textContent
+    const computerScore = parseInt(computerScoreText)
+    const newComputerScore = computerScore + 1
+    $('.scores .computer').textContent = newComputerScore
+    console.log('computer wins with score', newComputerScore)
+  }
+
+  if (player === 'scissors' && computer === 'rock') {
+    const computerScoreText = $('.scores .computer').textContent
+    const computerScore = parseInt(computerScoreText)
+    const newComputerScore = computerScore + 1
+    $('.scores .computer').textContent = newComputerScore
+    console.log('computer wins with score', newComputerScore)
+  }
+
+  // if (player === 'scissors' && computer === 'scissors') {
+  //   console.log('draw')
+  // }
+
+// HINT: Check for win, lose or draw, then call `gameOver()` eventually.
 }
 
 const getComputerMove = () => {
